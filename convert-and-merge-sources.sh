ee918901f2c57d509a587033eef7a4a9c26d3e0b
Sebastian Baltes <Sebastian.Baltes@uni-bayreuth.de>
2025-01-31 11:29:58 +0100

    Fix minor issues, add new macro for TODOs

--- convert-and-merge-sources.sh
+++ convert-and-merge-sources.sh
@@ -5,6 +5,7 @@ convert_tex_to_md() {
     for tex_file in "$directory"/*.tex; do
         [ -e "$tex_file" ] || continue
         md_file="${tex_file%.tex}.md"
+        # debug: --verbose --log="pandoc.log"
         pandoc --bibliography="literature.bib" --csl="ieee.csl" -s "$tex_file" -t markdown_strict --citeproc | perl -ne 'print if not /^---/.../^---/; END { print "\n" }' > "$md_file"
     done
 }
@@ -14,3 +15,4 @@ convert_tex_to_md "guidelines/_sources"
 
 cat guidelines/_sources/*.md > guidelines/index.md
 cat study-types/_sources/*.md > study-types/index.md
+
